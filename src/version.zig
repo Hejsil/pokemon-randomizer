13de1a0ff03f61b1a369b0d5deb004b2d26f1c07
Jimmi Holst Christensen <jhc@liab.dk>
2018-01-02 16:17:06 +0100

    Added version.zig

---
+++ src/version.zig
@@ -0,0 +1,125 @@
+const std = @import("std");
+const utils = @import("utils.zig");
+const nds = @import("nds/index.zig");
+const mem = std.mem;
+const debug = std.debug;
+const File = std.io.File;
+const Pair = utils.Pair;
+
+const NdsHeader = nds.header.Header;
+
+error UnknownVersion;
+
+pub const Version = enum {
+    Red,
+    Blue,
+    Yellow,
+
+    Gold,
+    Silver,
+    Crystal,
+
+    Ruby,
+    Sapphire,
+    Emerald,
+    FireRed,
+    LeafGreen,
+
+    Diamond,
+    Pearl,
+    Platinum,
+    HeartGold,
+    SoulSilver,
+
+    Black,
+    White,
+    Black2,
+    White2,
+
+    X,
+    Y,
+    OmegaRuby,
+    AlphaSapphire,
+
+    Sun,
+    Moon,
+    UltraSun,
+    UltraMoon,
+
+    pub fn fromFile(rom: &File) -> %Version {
+        const prev = %return rom.getPos();
+        defer _ = rom.seekTo(prev);
+
+        nds_block: {
+            var header : NdsHeader = undefined;
+            _ = %return rom.read(utils.asBytes(NdsHeader, &header));
+
+            return Version.fromNdsHeader(&header) %% break :nds_block;
+        }
+
+        // TODO: We only focus on NDS roms, for now. Add other roms later on
+
+        return error.UnknownVersion;
+    }
+
+    pub fn fromNdsHeader(header: &const NdsHeader) -> %Version {
+        const Game = Pair([]const u8, Version);
+        const games = []Game {
+            Game.init("IRB", Version.Black),
+            Game.init("IRA", Version.White),
+            Game.init("IRE", Version.Black2),
+            Game.init("IRD", Version.White2),
+            Game.init("ADA", Version.Diamond),
+            Game.init("APA", Version.Pearl),
+            Game.init("CPU", Version.Platinum),
+            Game.init("IPK", Version.HeartGold),
+            Game.init("IPG", Version.SoulSilver),
+        };
+
+        const game = loop: for (games) |game| {
+            if (mem.startsWith(u8, header.gamecode, game.first)) {
+                break :loop game.second;
+            }
+        } else {
+            return error.UnknownVersion;
+        };
+
+        // TODO: This is kinda ugly. Rewrite
+        const lang = switch (game.gen()) {
+            4 => lang_blk: {
+                const langs = "EFDSIJK";
+                break :lang_blk mem.indexOfScalar(u8, langs, header.gamecode[3]) ?? return error.UnknownVersion;
+            },
+            5 => lang_blk: {
+                const langs = "OFDSIJK";
+                break :lang_blk mem.indexOfScalar(u8, langs, header.gamecode[3]) ?? return error.UnknownVersion;
+            },
+            else => unreachable
+        };
+
+        return game;
+    }
+
+    pub fn gen(self: Version) -> u8 {
+        const V = Version;
+        return switch (self) {
+            V.Red,  V.Blue,     V.Yellow   => u8(1),
+            V.Gold, V.Silver,   V.Crystal  => u8(2),
+            V.Ruby, V.Sapphire, V.Emerald,
+            V.FireRed, V.LeafGreen,        => u8(3),
+            V.Diamond, V.Pearl, V.Platinum,
+            V.HeartGold, V.SoulSilver,     => u8(4),
+            V.Black,  V.White,
+            V.Black2, V.White2,            => u8(5),
+            V.X, V.Y, V.OmegaRuby, 
+            V.AlphaSapphire,               => u8(6),
+            V.Sun, V.Moon, V.UltraSun,
+            V.UltraMoon,                   => u8(7),
+            else                           => unreachable
+        };
+    }
+
+    pub fn hasPhysicalSpecialSplit(self: Version) -> bool {
+        return self.gen() > 3;
+    }
+};
