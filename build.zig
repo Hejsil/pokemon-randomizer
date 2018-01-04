e57e84cf19d8514ec6e3e6d7923e3a578fdf56d1
Jimmi Holst Christensen <jhc@liab.dk>
2018-01-04 09:15:58 +0100

    Removed ndstool build rom build.zig

--- build.zig
+++ build.zig
@@ -1,38 +1,8 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: &Builder) {
-    const ndstool = b.addCExecutable("ndstool.exe");
-    ndstool.linkSystemLibrary("stdc++");
-    ndstool.addCompileFlags([][]const u8 { "-DPACKAGE_VERSION=\"2.1.0\"" });
-
-    const files = [][]const u8 {
-        "banner.cpp",
-        "bigint.cpp",
-        "compile_date.c",
-        "crc.cpp",
-        "default_icon.c",
-        "elf.cpp",
-        "encryption.cpp",
-        "header.cpp",
-        "hook.cpp",
-        "loadme.c",
-        "logo.cpp",
-        "ndscodes.cpp",
-        "ndscreate.cpp",
-        "ndsextract.cpp",
-        "ndstool.cpp",
-        "ndstree.cpp",
-        "raster.cpp",
-        "sha1.cpp"
-    };
-
-    inline for (files) |file| {
-        ndstool.addSourceFile("ndstool/source/" ++ file);
-    }
-
     const randomizer = b.addExecutable("randomizer", "src/main.zig");
     randomizer.linkSystemLibrary("c");
-
-    b.default_step.dependOn(&ndstool.step);
+    
     b.default_step.dependOn(&randomizer.step);
 }
