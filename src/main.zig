687c9ce4f5acc2f0452250a199343c467e814b85
Jimmi Holst Christensen <rainbowhejsil@gmail.com>
2018-01-08 00:42:22 +0100

    Replaced %% with new catch keyword

--- src/main.zig
+++ src/main.zig
@@ -29,7 +29,7 @@ error NotARom;
 fn loadRom(file_path: []const u8, allocator: &mem.Allocator) -> %Rom {
     gba_blk: {
         var rom_file = %return io.File.openRead(file_path, null);
-        var rom = gen3.Game.fromFile(&rom_file, allocator) %% {
+        var rom = gen3.Game.fromFile(&rom_file, allocator) catch {
             rom_file.close();
             break :gba_blk;
         };
@@ -40,7 +40,7 @@ fn loadRom(file_path: []const u8, allocator: &mem.Allocator) -> %Rom {
 
     nds_blk: {
         var rom_file = %return io.File.openRead(file_path, null);
-        var rom = nds.Rom.fromFile(&rom_file, allocator) %% {
+        var rom = nds.Rom.fromFile(&rom_file, allocator) catch {
             rom_file.close();
             break :nds_blk;
         };
@@ -68,7 +68,7 @@ pub fn main() -> %void {
     const out_path = %return utils.first([]const u8, argsWithExe[2..]);
     const args = argsWithExe[3..];
 
-    var rom = loadRom(in_path, allocator) %% |err| {
+    var rom = loadRom(in_path, allocator) catch |err| {
         switch (err) {
             error.NotARom => {
                 %return stdout_stream.print("{} is not a rom.\n", in_path);
@@ -89,25 +89,25 @@ pub fn main() -> %void {
         Rom.Gba => |*gen3_rom| {
             var adapter = gen3.GameAdapter.init(gen3_rom);
             var random = rand.Rand.init(0);
-            randomizer.randomizeStats(&adapter.base, &random) %% |err| {
+            randomizer.randomizeStats(&adapter.base, &random) catch |err| {
                 %return stdout_stream.print("Couldn't randomize stats.\n");
                 return err;
             };
 
-            var out_file = io.File.openWrite(out_path, null) %% |err| {
+            var out_file = io.File.openWrite(out_path, null) catch |err| {
                 %return stdout_stream.print("Couldn't open {}.\n", out_path);
                 return err;
             };
             defer out_file.close();
 
             var file_stream = io.FileOutStream.init(&out_file);
-            gen3_rom.writeToStream(&file_stream.stream) %% |err| {
+            gen3_rom.writeToStream(&file_stream.stream) catch |err| {
                 %return stdout_stream.print("Unable to write gba to {}.\n", out_path);
                 return err;
             };
         },
         Rom.Nds => |*nds_rom| {
-            var before = io.File.openWrite("before", null) %% |err| {
+            var before = io.File.openWrite("before", null) catch |err| {
                 %return stdout_stream.print("Couldn't open {}.\n", out_path);
                 return err;
             };
@@ -117,18 +117,18 @@ pub fn main() -> %void {
             var before_stream = &before_file_stream.stream;
             %return nds_rom.header.prettyPrint(before_stream);
 
-            var out_file = io.File.openWrite(out_path, null) %% |err| {
+            var out_file = io.File.openWrite(out_path, null) catch |err| {
                 %return stdout_stream.print("Couldn't open {}.\n", out_path);
                 return err;
             };
             defer out_file.close();
 
-            nds_rom.writeToFile(&out_file) %% |err| {
+            nds_rom.writeToFile(&out_file) catch |err| {
                 %return stdout_stream.print("Unable to write nds to {}: {}\n", out_path, @errorName(err));
                 return err;
             };
 
-            var after = io.File.openWrite("after", null) %% |err| {
+            var after = io.File.openWrite("after", null) catch |err| {
                 %return stdout_stream.print("Couldn't open {}.\n", out_path);
                 return err;
             };
