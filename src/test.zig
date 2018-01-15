ec50c691f1b10ce30f3e04a417f7bf967b7b8f8e
Jimmi Holst Christensen <jhc@liab.dk>
2018-01-15 15:35:14 +0100

    Renamed index.zig to test.zig as it was only used for running tests

---
+++ src/test.zig
@@ -0,0 +1,10 @@
+test "" {
+    _ = @import("pokemon/index.zig");
+    _ = @import("ascii.zig");
+    _ = @import("clap.zig");
+    _ = @import("gba.zig");
+    _ = @import("little.zig");
+    _ = @import("nds.zig");
+    _ = @import("randomizer.zig");
+    _ = @import("utils.zig");
+}
