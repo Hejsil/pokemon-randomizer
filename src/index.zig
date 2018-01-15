b86dfc69e9469a3e13c9afd9676a99bfff6175e7
Jimmi Holst Christensen <jhc@liab.dk>
2018-01-15 15:33:19 +0100

    Added clap to index.zig

--- src/index.zig
+++ src/index.zig
@@ -4,6 +4,7 @@ pub const gba        = @import("gba.zig");
 pub const little     = @import("little.zig");
 pub const nds        = @import("nds.zig");
 pub const randomizer = @import("randomizer.zig");
+pub const clap       = @import("clap.zig");
 pub const utils      = @import("utils.zig");
 
 test "" {
@@ -13,5 +14,6 @@ test "" {
     _ = @import("little.zig");
     _ = @import("nds.zig");
     _ = @import("randomizer.zig");
+    _ = @import("clap.zig");
     _ = @import("utils.zig");
 }
