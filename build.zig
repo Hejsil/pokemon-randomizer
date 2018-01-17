212adc99ca7bfca1a83c07dff87dd5e9187cef5c
Jimmi Holst Christensen <jhc@liab.dk>
2018-01-17 09:32:15 +0100

    Updated build.zig to work with latest zig version

--- build.zig
+++ build.zig
@@ -1,8 +1,8 @@
 const Builder = @import("std").build.Builder;
 
-pub fn build(b: &Builder) {
+pub fn build(b: &Builder) -> %void {
     const randomizer = b.addExecutable("randomizer", "src/main.zig");
     randomizer.linkSystemLibrary("c");
-    
+
     b.default_step.dependOn(&randomizer.step);
 }
