00a79510c1be295e4a9112203bf8769d75a3433e
Jimmi Holst Christensen <jhc@liab.dk>
2018-01-25 09:14:59 +0100

    Inline loadRom into main

--- src/main.zig
+++ src/main.zig
@@ -19,6 +19,7 @@ var input_file  : []const u8 = "input";
 var output_file : []const u8 = "randomized";
 
 error InvalidOptions;
+error NotARom;
 
 fn setHelp(op: &randomizer.Options, str: []const u8) -> %void { help = true; }
 fn setInFile(op: &randomizer.Options, str: []const u8) -> %void { input_file = str; }
@@ -159,99 +160,55 @@ pub fn main() -> %void {
         return;
     }
 
-    var rom = loadRom(input_file, allocator) catch |err| {
-        switch (err) {
-            error.NotARom => {
-                try stdout_stream.print("{} is not a rom.\n", input_file);
-            },
-            else => {
-                // TODO: This could also be an allocation error.
-                //       Follow issue https://github.com/zig-lang/zig/issues/632
-                //       and refactor when we can check if error is part of an error set.
-                try stdout_stream.print("Unable to open {}.\n", input_file);
-            }
-        }
-
-        return err;
-    };
-    defer rom.destroy(allocator);
-
     var out_file = io.File.openWrite(output_file, null) catch |err| {
         try stdout_stream.print("Couldn't open {}.\n", output_file);
         return err;
     };
     defer out_file.close();
 
-    switch (rom) {
-        Rom.Gba => |gen3_rom| {
-            gen3_rom.validateData() catch |err| {
-                try stdout_stream.print("Warning: Invalid Pokemon game data. The rom will still be randomized, but there is no garenties that the rom will work as indented.\n");
 
-                switch (err) {
-                    error.NoBulbasaurFound => {
-                        try stdout_stream.print("Note: Pokemon 001 (Bulbasaur) did not have expected stats.\n");
-                        try stdout_stream.print("Note: If you are randomizing a hacked version, then .\n");
-                    },
-                    else => {}
-                }
-            };
-
-            var random = rand.Rand.init(0);
-            try randomizer.randomize(gen3_rom, options, &random, allocator);
-
-            var file_stream = io.FileOutStream.init(&out_file);
-            gen3_rom.writeToStream(&file_stream.stream) catch |err| {
-                try stdout_stream.print("Unable to write gba to {}.\n", output_file);
-                return err;
-            };
-        },
-        Rom.Nds => |nds_rom| {
-            nds_rom.writeToFile(&out_file) catch |err| {
-                try stdout_stream.print("Unable to write nds to {}\n", output_file);
-                return err;
-            };
-        },
-        else => {
-            try stdout_stream.print("Rom type not supported (yet)\n");
-        }
-    }
-}
-
-const Rom = union(enum) {
-    Gba: &gen3.Game,
-    Nds: &nds.Rom,
-
-    pub fn destroy(self: &const Rom, allocator: &mem.Allocator) {
-        switch (*self) {
-            Rom.Gba => |gen3_rom| gen3_rom.destroy(allocator),
-            Rom.Nds => |nds_rom|  nds_rom.destroy(allocator),
-        }
-    }
-};
-
-error NotARom;
-fn loadRom(file_path: []const u8, allocator: &mem.Allocator) -> %Rom {
     gba_blk: {
-        var rom_file = try io.File.openRead(file_path, null);
-        var rom = gen3.Game.fromFile(&rom_file, allocator) catch {
-            rom_file.close();
-            break :gba_blk;
-        };
-        rom_file.close();
+        var rom_file = try io.File.openRead(input_file, null);
+        //defer rom_file.close(); error: unreachable code
+        var game = gen3.Game.fromFile(&rom_file, allocator) catch break :gba_blk;
 
-        return Rom { .Gba = rom };
+        game.validateData() catch |err| {
+            try stdout_stream.print("Warning: Invalid Pokemon game data. The rom will still be randomized, but there is no garenties that the rom will work as indented.\n");
+
+            switch (err) {
+                error.NoBulbasaurFound => {
+                    try stdout_stream.print("Note: Pokemon 001 (Bulbasaur) did not have expected stats.\n");
+                    try stdout_stream.print("Note: If you are randomizing a hacked version, then .\n");
+                },
+                else => {}
+            }
+        };
+
+        var random = rand.Rand.init(0);
+        try randomizer.randomize(game, options, &random, allocator);
+
+        var file_stream = io.FileOutStream.init(&out_file);
+        game.writeToStream(&file_stream.stream) catch |err| {
+            try stdout_stream.print("Unable to write gba to {}.\n", output_file);
+            return err;
+        };
+
+        return;
     }
 
     nds_blk: {
-        var rom_file = try io.File.openRead(file_path, null);
-        var rom = nds.Rom.fromFile(&rom_file, allocator) catch {
-            rom_file.close();
-            break :nds_blk;
-        };
-        rom_file.close();
+        var rom_file = try io.File.openRead(input_file, null);
+        //defer rom_file.close(); error: unreachable code
+        var nds_rom = nds.Rom.fromFile(&rom_file, allocator) catch break :nds_blk;
 
-        return Rom { .Nds = rom };
+        nds_rom.writeToFile(&out_file) catch |err| {
+            try stdout_stream.print("Unable to write nds to {}\n", output_file);
+            return err;
+        };
+
+        return;
     }
 
+    try stdout_stream.print("Rom type not supported (yet)\n");
     return error.NotARom;
 }
