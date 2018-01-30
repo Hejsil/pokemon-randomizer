445f4ed1f41e2918d0a21c500da9a3f173a6cd03
Jimmi Holst Christensen <jhc@liab.dk>
2018-01-30 08:39:57 +0100

    Updated build script to be able to run tests. Sadly, we can't add package path to tests, so the tests fail

--- build.zig
+++ build.zig
@@ -4,4 +4,10 @@ pub fn build(b: &Builder) %void {
     const randomizer = b.addExecutable("randomizer", "src/main.zig");
     randomizer.addPackagePath("crc", "lib/zig-crc/crc.zig");
     b.default_step.dependOn(&randomizer.step);
+
+    // TODO: We need to be able to addPackagePath to tests, or else
+    //       tests wont be aple find "crc".
+    const tests = b.addTest("src/test.zig");
+    const test_step = b.step("test", "Run all tests");
+    test_step.dependOn(&tests.step);
 }
