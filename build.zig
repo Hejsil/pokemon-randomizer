bc22938fd137776939b3181745068482c6787cdb
Jimmi Holst Christensen <jhc@liab.dk>
2018-01-31 08:55:33 +0100

    Made the default build step build the randomizer, tools and run tests

--- build.zig
+++ build.zig
@@ -1,9 +1,10 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: &Builder) %void {
+    const randomizer_step = b.step("randomizer", "Build randomizer");
     const randomizer = b.addExecutable("randomizer", "src/main.zig");
     randomizer.addPackagePath("crc", "lib/zig-crc/crc.zig");
-    b.default_step.dependOn(&randomizer.step);
+    randomizer_step.dependOn(&randomizer.step);
 
     const tools_step = b.step("tools", "Build tools");
     const offset_finder = b.addExecutable("gen3-offset-finder", "tools/gen3-offset-finder/main.zig");
@@ -15,4 +16,8 @@ pub fn build(b: &Builder) %void {
     const tests = b.addTest("src/test.zig");
     const test_step = b.step("test", "Run all tests");
     test_step.dependOn(&tests.step);
+
+    b.default_step.dependOn(randomizer_step);
+    b.default_step.dependOn(tools_step);
+    b.default_step.dependOn(test_step);
 }
